e167ebbc3b4267faa764db82c94400237d7d7a90
MY COM <you@example.com>
2023-12-03 22:22:22 +0500

    utilization module 2nd page

--- Utilization/views.py
+++ Utilization/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def Utilization_1(request):
     return render(request, 'u11.html', context)
 def Utilization_2(request):
-    return render(request, 'u2.html', )
+    return render(request, 'u2.html', u2context)
 def Utilization_3(request):
     return render(request, 'u3.html', )
 def Utilization_4(request):
@@ -17,5 +17,10 @@ def Utilization_6(request):
 context = {
     'headers' : [
         'Utilization', 'Type', 'Date', 'Submitted', 'Refrence', 'Summary', 'Status',
+    ]
+}
+u2context = {
+    'headers' : [
+        'Product', 'Mfg#', 'Notes', 'Quantity', 'Cost', 'Availability', 'Substitutes',
     ]
 }
