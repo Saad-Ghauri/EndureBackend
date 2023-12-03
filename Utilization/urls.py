722e756cee6bc7f8f2df1bec640205d02c10be87
MY COM <you@example.com>
2023-12-03 19:37:00 +0500

    more Utilization module

--- Utilization/urls.py
+++ Utilization/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
   
     path('u1/', views.Utilization_1, name='u1'),
+    path('u2/', views.Utilization_2, name='u2'),
+    path('u3/', views.Utilization_3, name='u3'),
+    path('u4/', views.Utilization_4, name='u4'),
+    path('u5/', views.Utilization_5, name='u5'),
+    path('u6/', views.Utilization_6, name='u6'),
 ]
--- Utilization/views.py
+++ Utilization/views.py
@@ -2,4 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def Utilization_1(request):
-    return render(request, 'u11.html')
+    return render(request, 'u11.html', context)
+def Utilization_2(request):
+    return render(request, 'u2.html', )
+def Utilization_3(request):
+    return render(request, 'u3.html', )
+def Utilization_4(request):
+    return render(request, 'u4.html', )
+def Utilization_5(request):
+    return render(request, 'u5.html', )
+def Utilization_6(request):
+    return render(request, 'u6.html', )
+
+context = {
+    'headers' : [
+        'Utilization', 'Type', 'Date', 'Submitted', 'Refrence', 'Summary', 'Status',
+    ]
+}
