6686bc0447dda612c0ee0ccdd3689d20a0cd0c94
MY COM <you@example.com>
2023-12-06 20:43:13 +0500

    deployed

--- Utilization/views.py
+++ Utilization/views.py
@@ -6,13 +6,13 @@ def Utilization_1(request):
 def Utilization_2(request):
     return render(request, 'u2.html', u2context)
 def Utilization_3(request):
-    return render(request, 'u3.html', )
+    return render(request, 'u3.html', u3headers)
 def Utilization_4(request):
-    return render(request, 'u4.html', )
+    return render(request, 'u4.html', u4headers)
 def Utilization_5(request):
-    return render(request, 'u5.html', )
+    return render(request, 'u5.html', u5headers)
 def Utilization_6(request):
-    return render(request, 'u6.html', )
+    return render(request, 'u6.html', u6headers)
 
 context = {
     'headers' : [
@@ -24,3 +24,19 @@ u2context = {
         'Product', 'Mfg#', 'Notes', 'Quantity', 'Cost', 'Availability', 'Substitutes',
     ]
 }
+
+
+u3headers = {
+    'headers': [ 'Utilization', 'Date', 'Case', 'Refrence', 'Submitted', 'Items', 'Status']   
+  }
+
+
+u4headers = {
+    'headers': [ 'Utilization', 'Date', 'Type', 'Submitted', 'Reference', 'Summary', 'Status']   
+  }
+u5headers = {
+    'headers': [ 'Utilization', 'Date', 'Reference', 'Submitted', 'Case Summary', 'Items', 'Status']   
+  }
+u6headers = {
+    'headers': [ 'Utilization', 'Date', 'Reference', 'Submitted', 'Case Summary', 'Items', 'Status']   
+  }
